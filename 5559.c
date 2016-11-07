51ba3739e7963835f1b4c01b48a8b07278c4c125
Soo-Hyun Choi <soohyunc@users.noreply.github.com>
2016-11-08 02:09:57 +0900

    JOI 깃발

--- 5559.c
+++ 5559.c
@@ -13,19 +13,59 @@ typedef struct _n {
 	int m;
 	int n;
 } BLANK;
-BLANK spot[Max];
+BLANK spot[Max], input[Max];
 
 int N, M;
 int R;		// number of '?'
+int count;
 
 char flag[Max][Max];
 
 void __init__() {
+	count = 0;
 	for (int i = 0; i < Max; i++) {
 		spot[i].m = spot[i].n = 0;
+		input[i].m = input[i].n = 0;
+
 		for (int j = 0; j < Max; j++) {
 			flag[i][j] = '0';
 		}
+	}
+}
+
+void swap(int a, int b) {
+	int tm = input[a].m;
+	int tn = input[a].n;
+
+	input[a].m = input[b].m;
+	input[a].n = input[b].n;
+
+	input[b].m = tm;
+	input[b].n = tn;
+}
+
+void rollout() {
+	
+}
+
+void _print() {
+	for (int i = 0; i < R; i++) {
+		printf("(%d %d) ", spot[i].m, spot[i].n);
+	} printf("\n");
+}
+
+void PI(int n, int r) {
+	if (r == 0) {
+		_print();
+		//rollout();
+		return;
+	}
+
+	for (int i = 0; i < n; i++) {
+		swap(i, n - 1);
+		spot[r - 1] = input[n - 1];
+		PI(n - 1, r - 1);
+		swap(i, n - 1);
 	}
 }
 
@@ -38,14 +78,16 @@ int main() {
 	for (int i = 0; i < M; i++) {
 		for (int j = 0; j < N; j++) {
 			if (flag[i][j] == '?') {
-				spot[R].m = i;
-				spot[R].n = j;
+				input[R].m = i;
+				input[R].n = j;
 				R++;
 			}
 		}
 	} //printf("%d\n", R);
 
-	perm(4, R);
 
+	PI(3, R);
+
+	printf("%d\n", count%100000);
 	return 0;
 }
