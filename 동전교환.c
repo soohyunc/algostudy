ded11012504837754195a4cd60e137fca2371076
Soo-Hyun Choi <soohyunc@users.noreply.github.com>
2016-12-03 20:27:27 +0900

    동전교환

--- 동전교환.c
+++ 동전교환.c
@@ -1,57 +1,18 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-#define N   10
-#define MAX_CNT 64001
+#define NUM 10
+#define MAX 64001
 
-int COIN[N];
-int NumCoin[MAX_CNT];
+int val[NUM];
+int N[MAX];
 
-int partition(int a[], int p, int r);
-void quickSort(int a[], int p, int r);
-
-int minCnt = MAX_CNT;
-int numCoin = 0;
-
-void main() {
-	int i, j;
-	int money = 0;
-	int ret = 0;
-
-	scanf("%d", &numCoin);
-
-	for (i = 0; i < numCoin; i++) {
-		scanf("%d", &COIN[i]);
-	}
-	scanf("%d", &money);
-
-	quickSort(COIN, 0, numCoin - 1);
-
-	for (i = 1; i < MAX_CNT; i++)
-	{
-		NumCoin[i] = MAX_CNT;
-		for (j = 00; j < numCoin; j++) {
-			if (i < COIN[j])
-				break;
-			if (NumCoin[i] > NumCoin[i - COIN[j]] + 1)
-				NumCoin[i] = NumCoin[i - COIN[j]] + 1;
-		}
-	}
-
-	if (NumCoin[money] == MAX_CNT)
-		printf("impossible\n");
-	else
-		printf("%d\n", NumCoin[money]);
-}
-
-int partition(int a[], int p, int r)
-{
+int partition(int a[], int p, int r) {
 	int tmp = 0;
 	int pivot = a[r];
 	int smallPos = p - 1;
-	int i;
 
-	for (i = p; i < r; i++) {
+	for (int i = p; i < r; i++) {
 		if (pivot >= a[i]) {
 			tmp = a[i];
 			a[i] = a[++smallPos];
@@ -66,11 +27,42 @@ int partition(int a[], int p, int r)
 	return smallPos;
 }
 
-void quickSort(int a[], int p, int r)
-{
+void quickSort(int a[], int p, int r) {
 	if (p < r) {
 		int q = partition(a, p, r);
 		quickSort(a, p, q - 1);
 		quickSort(a, q + 1, r);
 	}
 }
+
+int main(void) {
+	int numCoin = 0;
+	int money = 0;
+
+	scanf("%d", &numCoin);
+	for (int i = 0; i < numCoin; i++) {
+		scanf("%d", &val[i]);
+	}
+	scanf("%d", &money);
+
+	// optional (i.e., you can simply apply bubblesort)
+	quickSort(val, 0, numCoin - 1);
+
+	for (int i = 1; i < MAX; i++) {
+		N[i] = MAX;
+		for (int j = 0; j < numCoin; j++) {
+			if (i < val[j])
+				break;
+			if (N[i] > N[i - val[j]] + 1)
+				N[i] = N[i - val[j]] + 1;
+		}
+	}
+
+	if (N[money] == MAX)
+		printf("impossible\n");
+	else
+		printf("%d\n", N[money]);
+
+	return 0;
+}
+
